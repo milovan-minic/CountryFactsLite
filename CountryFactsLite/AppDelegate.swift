e538b6c683e7487214cc12731714538c732538f3
Lazar <lakiando@outlook.com>
2016-11-27 16:45:59 +0100

    in AppDelegate created instance of storyboard.

--- CountryFactsLite/AppDelegate.swift
+++ CountryFactsLite/AppDelegate.swift
@@ -15,12 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         
+        let storyboard = UIStoryboard(name: "CountryFactsLite", bundle: nil)
+        guard let nc = storyboard.instantiateInitialViewController() as? UINavigationController else {
+            fatalError("Failed to create instance of UINC from \(storyboard)")
+        }
+        
+        self.window?.rootViewController = nc
         return true
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window?.makeKeyAndVisible()
         return true
     }
 
