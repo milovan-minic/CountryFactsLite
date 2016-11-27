0065169dc610cccde426b5b8369823df47cea8a9
Lazar <lakiando@outlook.com>
2016-11-27 16:52:06 +0100

    crated new cocoa touch file and named CountryFactsController
    This file is sub class from view controller, and i connetct to view controller from storyboard,
    in indentity inspector in custom class.

---
+++ CountryFactsLite/CountryFactsController.swift
@@ -0,0 +1,18 @@
+//
+//  CountryFactsController.swift
+//  CountryFactsLite
+//
+//  Created by iosakademija on 11/27/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import UIKit
+
+class CountryFactsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
