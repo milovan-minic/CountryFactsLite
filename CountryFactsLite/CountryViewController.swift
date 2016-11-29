3dc4f0771ac9dcbc4ed75d007baaf11c680be167
Milovan Minic <milovan.minic@gmail.com>
2016-11-29 18:07:15 +0100

    Added new CountryViewController and connected it to CountryFactsLite.storyboard

---
+++ CountryFactsLite/CountryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CountryViewController.swift
+//  CountryFactsLite
+//
+//  Created by iosakademija on 11/29/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import UIKit
+
+class CountryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
